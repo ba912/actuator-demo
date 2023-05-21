eb10951e2a8fbf0fbd3091bf07258ad9ff553d4e
ba912 <a921003@naver.com>
2023-05-21 18:56:21 +0900

    initialize project

---
+++ src/main/kotlin/com/example/actuatordeme/ActuatorDemeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.actuatordeme
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ActuatorDemeApplication
+
+fun main(args: Array<String>) {
+    runApplication<ActuatorDemeApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/actuatordeme/ActuatorDemeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.actuatordeme
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ActuatorDemeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
