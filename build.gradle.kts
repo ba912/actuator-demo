cc75c90eed7982f75c22c37ab4e72b7a0f92f978
ba912 <a921003@naver.com>
2023-05-27 19:00:45 +0900

    chore: add git-properties plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,8 @@ plugins {
     id("io.spring.dependency-management") version "1.1.0"
     kotlin("jvm") version "1.8.21"
     kotlin("plugin.spring") version "1.8.21"
+    id ("com.gorylenko.gradle-git-properties") version "2.4.1"
+
 }
 
 group = "com.example"
